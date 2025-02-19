a325c387314ae9455034e4175ec2d73cb8e0ce63
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2025-02-19 22:38:02 +0530

    Create Database.java

---
+++ Database.java
@@ -0,0 +1,51 @@
+class Database {
+    String data;
+
+    Database() {
+        // Name,Age,Country,Skill,Matches,Innings,Runs,Wickets,ICC Ranking,Price
+        data = """
+                Virat Kohli,35,IND,BT,275,245,12345,20,1,40
+                Rohit Sharma,36,IND,BT,245,215,10500,10,3,35
+                Steve Smith,34,AUS,BT,133,122,8000,0,23,14
+                David Warner,37,AUS,BT,128,117,7500,0,38,16
+                Babar Azam,29,PAK,BT,110,105,6500,0,5,10
+                Ruturaj Gaikwad,27,IND,BT,35,33,1400,0,25,15
+                Kane Williamson,34,NZ,BT,112,104,6200,0,7,14
+                Harry Brook,25,ENG,BT,30,28,1200,0,12,23
+                Travis Head,30,AUS,BT,55,52,2300,0,22,16
+                Suryakumar Yadav,33,IND,BT,85,75,4500,0,6,16
+                Shubman Gill,24,IND,BT,50,45,2100,0,13,30
+                Fakhar Zaman,33,PAK,BT,85,80,3400,0,20,13
+                Liam Livingstone,31,ENG,BT,45,40,1800,0,18,12
+                Shreyas Iyer,29,IND,BT,60,55,2200,0,22,20
+                Rishabh Pant,26,IND,BT,75,70,3000,0,15,17
+                Dinesh Karthik,39,IND,BT,89,80,2800,0,25,1
+                Martin Guptill,37,NZ,BT,200,190,7500,0,11,16
+                Chris Gayle,45,WI,BT,300,280,11000,0,42,21
+                Imam-ul-Haq,28,PAK,BT,70,65,2900,0,28,14
+                Tom Latham,32,NZ,BT,75,70,3500,0,21,17
+                Shaheen Afridi,23,PAK,BL,35,28,400,80,9,11
+                Pat Cummins,31,AUS,BL,64,59,1200,350,16,20
+                Jasprit Bumrah,30,IND,BL,60,50,800,120,8,42
+                Trent Boult,35,NZ,BL,70,66,900,150,10,19
+                Kagiso Rabada,29,SA,BL,50,48,300,150,12,17
+                Rashid Khan,25,AFG,BL,70,65,1700,300,4,18
+                Yuzvendra Chahal,33,IND,BL,45,42,1200,140,24,20
+                Mohammed Shami,33,IND,BL,60,58,600,180,19,18
+                Sunil Narine,35,WI,BL,70,61,3450,180,26,18
+                Kyle Jamieson,29,NZ,BL,20,19,250,60,27,10
+                Ben Stokes,32,ENG,AR,104,86,4000,150,17,25
+                Shakib Al Hasan,37,BAN,AR,300,250,6000,400,29,20
+                Hardik Pandya,30,IND,AR,75,60,2000,60,31,26
+                Andre Russell,35,WI,AR,80,70,2500,100,37,25
+                Jason Holder,32,WI,AR,70,65,1800,110,39,23
+                Mohammad Nabi,38,AFG,AR,100,80,3000,150,29,22
+                Jos Buttler,33,ENG,WK,150,130,5000,0,49,15
+                MS Dhoni,43,IND,WK,350,330,10000,0,47,38
+                Kumar Sangakkara,47,SRI,WK,404,380,1500,0,10,20
+                Quinton de Kock,31,SA,WK,190,170,5500,0,14,22
+                Ben Foakes,30,ENG,WK,60,55,2200,0,20,12
+                AB De Villiers,42,SA,WK,180,171,9500,0,66,15
+                """;
+    }
+}
